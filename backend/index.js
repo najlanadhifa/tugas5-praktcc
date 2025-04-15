167649bbc0751fab65092b269498756163bb8693
Difa <najlanadhifa1103@gmail.com>
2025-04-15 15:52:20 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import route from "./routes/route.js";
 
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT | 5000;
 
 app.use(cors());
 app.use(express.json());
